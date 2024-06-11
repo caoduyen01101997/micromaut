31072bb7d2f396f8c4a5687a479ed9366aad34c0
duyenpnc <duyenpnc@vnpt.vn>
2024-06-11 10:51:03 +0700

    git commit bổ sung thêm vài thu71

--- src/main/java/com/example/document/Blog.java
+++ src/main/java/com/example/document/Blog.java
@@ -1,25 +1,25 @@
 package com.example.document;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 import java.io.Serializable;
 import java.util.Date;
 
 import io.micronaut.serde.annotation.Serdeable;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Serdeable
 @Entity(name = "blog")
 public class Blog implements Serializable {
 
+
     @Id
-    @GeneratedValue
-    private Long id;
+    @NotNull
+    private  Long id;
 
     private String author;
 
@@ -40,6 +40,10 @@ public class Blog implements Serializable {
     @Column(name = "timetoread")
     private int timeToRead;
 
+    public Blog(Long id) {
+        this.id = id;
+    }
+
     @PrePersist
     protected void onCreate() {
         updatedDate = createdDate = new Date();
@@ -48,5 +52,69 @@ public class Blog implements Serializable {
     @PreUpdate
     protected void onUpdate() {
         updatedDate = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public int getTimeToRead() {
+        return timeToRead;
+    }
+
+    public void setTimeToRead(int timeToRead) {
+        this.timeToRead = timeToRead;
     }
 }

cb339bacdeee7871075c17288e9c51288a4837e2
duyenpnc <caoduyen01.ag@gmail.com>
2024-08-14 22:13:11 +0700

    Commit them token cho app

--- src/main/java/com/example/document/User.java
+++ src/main/java/com/example/document/User.java
@@ -2,18 +2,12 @@ package com.example.document;
 
 import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
-import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.serde.annotation.Serdeable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import java.util.Date;
+
 
 @Builder
 @AllArgsConstructor
